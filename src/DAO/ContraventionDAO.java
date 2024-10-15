1cde1c9b46ec7822c4122ba46b32e33a234ad1f7
bonito85 <bonituynathan@gmail.com>
2024-10-15 18:34:21 +0000

    Modification de la classe ContraventionDAO

--- src/DAO/ContraventionDAO.java
+++ src/DAO/ContraventionDAO.java
@@ -1,10 +1,12 @@
 package DAO;
 
+import Exceptions.InvalidInputException;
 import models.Contravention;
+import models.Contrevenant;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContraventionDAO {
     private Connection connection;
@@ -23,10 +25,60 @@ public class ContraventionDAO {
             stmt.setString(3, String.valueOf(contravention.getAgentId()));
             stmt.setString(1, String.valueOf(contravention.getInfractionId()));
             stmt.setString(1, String.valueOf(contravention.getContrevenantId()));
-
+            stmt.executeUpdate();
         }
     }
     // Lire les contraventions par l'id
+    public Contravention getContraventionById(int id) throws SQLException, InvalidInputException {
+        String query = "SELECT * FROM contraventions WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Contravention(rs.getString("date"), rs.getString("lieu"), rs.getInt("agentId"), rs.getInt("infractionId"), rs.getInt("contrevenantId"));
+            }
+        }
+        return null;
+    }
+
+    //Obtenir toutes les contraventions
+
+    public List<Contravention> getAllContraventions() throws SQLException, InvalidInputException {
+        List<Contravention> contraventions = new ArrayList<>();
+        String query = "SELECT * FROM contraventions";
+        try (Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while(rs.next())  {
+                contraventions.add(new Contravention(rs.getString("date"), rs.getString("lieu"), rs.getInt("agentId"), rs.getInt("infractionId"), rs.getInt("contrevenantId")));
+            }
+        }
+        return contraventions;
+    }
+
+    //Mettre a jour une contravention
+
+    public void mettreAJourContravention(Contravention contravention, int id) throws SQLException {
+        String query = "UPDATE contraventions SET date = ?, lieu = ?, agentId = ?, infractionId = ?, contrevenantId = ? WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, contravention.getDate());
+            stmt.setString(2, contravention.getLieu());
+            stmt.setString(3, String.valueOf(contravention.getAgentId()));
+            stmt.setString(4, String.valueOf(contravention.getInfractionId()));
+            stmt.setString(5, String.valueOf(contravention.getContrevenantId()));
+            stmt.setInt(6, id);
+            stmt.executeUpdate();
+        }
+    }
+
+    // Suprimer un agent une contravention
+
+    public void supprimerContravention(int id) throws SQLException {
+        String query = "DELETE FROM contraventions WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
 
 
 }
