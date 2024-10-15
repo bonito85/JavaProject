e306a0acf8ec3bdb284e07b55a4143f9ac467585
bonito85 <bonituynathan@gmail.com>
2024-10-15 18:34:56 +0000

    Mise a jour de la classe Paiement

--- src/models/Paiement.java
+++ src/models/Paiement.java
@@ -5,14 +5,20 @@ public class Paiement {
     private String datePaiement;
     private double montant;
     private boolean esPaye;
+    private Contravention contraventionId;
 
     //Constructeur de la classe Paiement
 
-    public Paiement(String datePaiement, double montant, boolean esPaye) {
+    public Paiement(String datePaiement, double montant, boolean esPaye, Contravention contraventionId) {
         this.datePaiement = datePaiement;
         this.montant = montant;
+        this.contraventionId = contraventionId;
         this.esPaye = esPaye;
     }
+
+    public Paiement(String date, double montantPaiement, int contraventionId) {
+    }
+
     //Getters et les setters
     public boolean isEsPaye() {
         return esPaye;
@@ -35,5 +41,13 @@ public class Paiement {
 
     public void setMontant(double montant) {
         this.montant = montant;
+    }
+
+    public Contravention getContraventionId() {
+        return contraventionId;
+    }
+
+    public void setContraventionId(Contravention contraventionId) {
+        this.contraventionId = contraventionId;
     }
 }
