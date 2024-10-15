181dc164a7f8cbcaca5b5ce9d71ef2c165cfc355
bonito85 <bonituynathan@gmail.com>
2024-10-15 19:36:02 +0000

    Ajout du fichier Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import DAO.ContrevenantDAO;
+import Exceptions.InvalidInputException;
+import models.Contrevenant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi everyone");
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jbdc:mysql://127.0.0.1:3306/gestionnaire_contravention",
+                    "root",
+                    "mysql@123"
+            );
+
+            // Creation
+            try {
+                Contrevenant contrevenant = new Contrevenant("Michael", "Ouagadougou", "A0023");
+                ContrevenantDAO contrevenantDAO = new ContrevenantDAO(connection);
+                System.out.println("Le contrevenant a ete ajoutée avec success :)");
+
+            } catch (InvalidInputException e) {
+                System.out.println("Erreur : " + e.getMessage());
+            }
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
