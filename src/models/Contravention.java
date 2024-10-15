c6e059a7cf0236874e5a2b22f06b968297f1b2a3
bonito85 <bonituynathan@gmail.com>
2024-10-15 18:35:22 +0000

    Mise a jour de la classe Contravention

--- src/models/Contravention.java
+++ src/models/Contravention.java
@@ -4,19 +4,63 @@ public class Contravention {
     private int id;
     private String date;
     private String lieu;
-    private AgentDePolice agentId;
-    private Infraction infractionId;
-    private Contrevenant contrevenantId;
+    private AgentDePolice agent ;
+    private Infraction infraction;
+    private Contrevenant contrevenant;
+    private int agentId;
+    private int infractionId;
+    private int contrevenantId;
 
 
     //Constructeur de la classe Contrevenant
     public Contravention(String date, String lieu, AgentDePolice agent, Infraction infraction, Contrevenant contrevenant) {
         this.date = date;
         this.lieu = lieu;
-        this.agentId = agent;
-        this.infractionId= infractionId;
+        this.agent = agent;
+        this.infraction = infraction;
+        this.contrevenant = contrevenant ;
+    }
+
+    public Contravention(String date, String lieu, int agentId, int infractionId, int contrevenatId) {
+        this.date = date;
+        this.lieu = lieu;
+        this.agentId = agentId;
+        this.infractionId = infractionId;
+        this.contrevenantId = contrevenatId;
+    }
+    //--------------------------------------------------------------------------------
+
+    /*
+    * Getters et les setters pour le second constructeur
+
+    * */
+
+    public int getAgentId() {
+        return agentId;
+    }
+
+    public void setAgentId(int agentId) {
+        this.agentId = agentId;
+    }
+
+    public void setInfractionId(int infractionId) {
+        this.infractionId = infractionId;
+    }
+
+    public int getInfractionId() {
+        return infractionId;
+    }
+
+    public int getContrevenantId() {
+        return contrevenantId;
+    }
+
+    public void setContrevenantId(int contrevenantId) {
         this.contrevenantId = contrevenantId;
     }
+    //------------------------------------------------------------------------------------
+
+
 //Getters et Setters
 
 
@@ -41,27 +85,29 @@ public class Contravention {
     }
 
 
-    public AgentDePolice getAgentId() {
-        return agentId;
+    public AgentDePolice getAgent() {
+        return agent;
     }
 
-    public void setAgentId(AgentDePolice agentId) {
-        this.agentId = agentId;
+    public void setAgentId(AgentDePolice agent) {
+        this.agent = agent;
     }
 
-    public Infraction getInfractionId() {
-        return infractionId;
+    public Infraction getInfraction() {
+        return infraction;
     }
 
-    public void setInfractionId(Infraction infractionId) {
-        this.infractionId = infractionId;
+    public void setInfractionId(Infraction infraction) {
+
+        this.infraction = infraction;
     }
 
-    public Contrevenant getContrevenantId() {
-        return contrevenantId;
+    public Contrevenant getContrevenant() {
+        return contrevenant;
     }
 
-    public void setContrevenantId(Contrevenant contrevenantId) {
-        this.contrevenantId = contrevenantId;
+    public void setContrevenant(Contrevenant contrevenant) {
+        this.contrevenant = contrevenant;
     }
+
 }
